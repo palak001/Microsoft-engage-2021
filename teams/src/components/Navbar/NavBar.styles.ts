f73cd5d0a83096e54b267e2cfcbfa4d3f0112932
palak001 <palak7372@gmail.com>
2021-06-12 14:42:34 +0530

    defactoring of code

--- teams/src/components/Navbar/NavBar.styles.ts
+++ teams/src/components/Navbar/NavBar.styles.ts
@@ -1,18 +1,47 @@
-import { IStackProps } from '@fluentui/react';
+import {
+  IIconProps,
+  IPersonaSharedProps,
+  ISearchBoxProps,
+  IStackProps,
+} from "@fluentui/react";
 
 export const NavBarStackProps: IStackProps = {
-    tokens: {
-        padding: '10px',
+  tokens: {
+    padding: "10px",
+  },
+  verticalAlign: "center",
+  horizontalAlign: "space-between",
+  styles: {
+    root: {
+      background: "#464775",
+      height: "50px",
+      color: "white",
+      fontFamily:
+        '"Segoe UI",system-ui,"Apple Color Emoji","Segoe UI Emoji",sans-serif',
+      fontSize: "14px",
     },
-    verticalAlign: 'center',
-    horizontalAlign: 'space-between',
-    styles: {
-        root: {
-            background: '#464775',
-            height: '50px',
-            color: 'white',
-            fontFamily: '"Segoe UI",system-ui,"Apple Color Emoji","Segoe UI Emoji",sans-serif',
-            fontSize: '14px'
-        }
-    }
-}
+  },
+};
+
+export const IconProps: IIconProps = {
+  styles: {
+    root: {
+      fontSize: "20px",
+      width: "20px",
+    },
+  },
+};
+
+export const SearchProps: ISearchBoxProps = {
+  placeholder: "Search",
+
+  styles: {
+    root: {
+      width: "900px",
+    },
+  },
+};
+
+export const PersonaProps: IPersonaSharedProps = {
+  imageInitials: "P",
+};
--- teams/src/components/Navbar/NavBar.tsx
+++ teams/src/components/Navbar/NavBar.tsx
@@ -1,53 +1,45 @@
-import React from 'react';
-import { initializeIcons } from '@fluentui/font-icons-mdl2';
-import { Icon, IIconProps } from '@fluentui/react/lib/Icon';
-
-import { IPersonaSharedProps, ISearchBoxProps, Persona, PersonaPresence, PersonaSize, SearchBox, Stack } from '@fluentui/react';
-import { NavBarStackProps } from './NavBar.styles';
-
-const IconProps: IIconProps = {
-    styles: {
-        root: {
-            fontSize: '20px',
-            width: '20px'
-        }
-    }
-}
-
-const SearchProps: ISearchBoxProps = {
-    placeholder: 'Search',
-    
-    styles: {
-        root: {
-            width: '900px'
-        }
-    }
-}
-
-const PersonaProps: IPersonaSharedProps = {
-    imageInitials: 'P',
-}
+import React from "react";
+import { initializeIcons } from "@fluentui/font-icons-mdl2";
+import { Icon } from "@fluentui/react/lib/Icon";
+import {
+  Persona,
+  PersonaPresence,
+  PersonaSize,
+  SearchBox,
+  Stack,
+} from "@fluentui/react";
+import {
+  IconProps,
+  NavBarStackProps,
+  PersonaProps,
+  SearchProps,
+} from "./NavBar.styles";
 
 export const NavBar: React.FunctionComponent = () => {
-    initializeIcons();
-    return(
-        <Stack horizontal {...NavBarStackProps}>
-            <Stack horizontal tokens={{childrenGap: '10px'}}>
-                <Stack><Icon iconName={'WaffleOffice365'} {...IconProps} /></Stack>
-                <Stack>Microsoft Teams </Stack>
-            </Stack>
-            <Stack>
-              <SearchBox {...SearchProps } onSearch={newValue => console.log('value is ' + newValue)} />
-            </Stack>
-            <Stack horizontal tokens={{childrenGap: '10px'}}>
-                <Icon iconName={'more'} {...IconProps} />
-                <Persona
-                    {...PersonaProps}
-                    size={PersonaSize.size32}
-                    presence={PersonaPresence.online}
-                    imageAlt="Palak, status is online"
-                />
-            </Stack>
+  initializeIcons();
+  return (
+    <Stack horizontal {...NavBarStackProps}>
+      <Stack horizontal tokens={{ childrenGap: "10px" }}>
+        <Stack>
+          <Icon iconName={"WaffleOffice365"} {...IconProps} />
         </Stack>
-    )
-}
+        <Stack>Microsoft Teams </Stack>
+      </Stack>
+      <Stack>
+        <SearchBox
+          {...SearchProps}
+          onSearch={(newValue) => console.log("value is " + newValue)}
+        />
+      </Stack>
+      <Stack horizontal tokens={{ childrenGap: "10px" }}>
+        <Icon iconName={"more"} {...IconProps} />
+        <Persona
+          {...PersonaProps}
+          size={PersonaSize.size32}
+          presence={PersonaPresence.online}
+          imageAlt="Palak, status is online"
+        />
+      </Stack>
+    </Stack>
+  );
+};
