a84c8bac41581074392f8c7169538637a7314c4e
palak001 <palak7372@gmail.com>
2021-06-19 10:26:24 +0530

    minor cors related bug

--- server.js
+++ server.js
@@ -4,45 +4,16 @@ const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
-
-app.use(
-  cors({
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-    methods: "GET, PUT",
-  })
-);
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 const io = require("socket.io")(server, {
   cors: {
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
+
+app.use(cors());
+const PORT = process.env.PORT || 8000;
 
 // socket.emit => only to that particular socket will receive the event
 // io.emit => all the sockets connected to the io will revieve this
@@ -70,8 +41,8 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/", cors, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-server.listen(PORT, () => console.log("Server is running on port 8000"));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
